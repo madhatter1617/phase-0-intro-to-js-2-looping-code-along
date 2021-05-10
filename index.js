7877dcd646959cd7bb88676036e5f3ee65eddee4
Rosa <rosarenee@att.net>
2021-05-09 23:39:47 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+//const  Cards= [ 'Lisa', 'Kaitlin', 'Jan' ];
+const gift ='surprise';
+
+function writeCards(Cards,gift) {
+let newA = []; 
+
+for (let n = 0; n < Cards.length; n++){
+  newA.push(`Thank you, ${Cards[n]}, for the wonderful ${gift} gift!`);
+debugger;
+}
+return newA;
+}
+writeCards(newA);
+
+function countDown(){
+let num  = 11;
+while (num > 0) {
+  num--;
+  console.log(num);
+  
+} 
+}
